7789f50f7b725c88092e467d484eeca74f1ce683
Tong Zhang <ztong0001@gmail.com>
2021-02-07 23:29:33 -0500

    hpsa

--- aplib/hw_model_hpsa.h
+++ aplib/hw_model_hpsa.h
@@ -12,22 +12,58 @@
   if (probe_len < 10000000) {                                                  \
     switch (addr) {                                                            \
     case (0x0): {                                                              \
-      *((uint32_t *)dest) = 0x2231894d;                                        \
+      *((uint8_t *)dest) = 'C';                                                \
       break;                                                                   \
     }                                                                          \
-    case (0x4): {                                                              \
-      *((uint32_t *)dest) = 0x196c6148;                                        \
+    case (0x1): {                                                              \
+      *((uint8_t *)dest) = 'I';                                                \
       break;                                                                   \
     }                                                                          \
-    case (0x24): {                                                             \
-      *((uint32_t *)dest) = 0x80000000UL;                                      \
+    case (0x2): {                                                              \
+      *((uint8_t *)dest) = 'S';                                                \
+      break;                                                                   \
+    }                                                                          \
+    case (0x3): {                                                              \
+      *((uint8_t *)dest) = 'S';                                                \
+      break;                                                                   \
+    }                                                                          \
+    case (0x8): {                                                              \
+      *((uint32_t *)dest) = 0xffffffff;                                        \
+      break;                                                                   \
+    }                                                                          \
+    case (0xc): {                                                              \
+      *((uint32_t *)dest) = 0x00000006l;                                       \
       break;                                                                   \
     }                                                                          \
     case (0x20): {                                                             \
-      *((uint32_t *)dest) = 0x0;                                               \
+      *((uint32_t *)dest) = 0x2;                                               \
       break;                                                                   \
     }                                                                          \
-    case (0x28): {                                                             \
+    case (0x34): {                                                             \
+      *((uint32_t *)dest) = 0x8;                                               \
+      break;                                                                   \
+    }                                                                          \
+    case (0x40): {                                                             \
+      *((uint32_t *)dest) = 0x1;                                               \
+      break;                                                                   \
+    }                                                                          \
+    case (0x44): {                                                             \
+      *((uint32_t *)dest) = 0x10;                                              \
+      break;                                                                   \
+    }                                                                          \
+    case (0x54): {                                                             \
+      *((uint32_t *)dest) = 0x10;                                              \
+      break;                                                                   \
+    }                                                                          \
+    case (0xb0): {                                                             \
+      *((uint32_t *)dest) = 0xffff0000;                                        \
+      break;                                                                   \
+    }                                                                          \
+    case (0xb4): {                                                             \
+      *((uint32_t *)dest) = 0x10;                                              \
+      break;                                                                   \
+    }                                                                          \
+    case (0xb8): {                                                             \
       *((uint32_t *)dest) = 0x0;                                               \
       break;                                                                   \
     }                                                                          \
