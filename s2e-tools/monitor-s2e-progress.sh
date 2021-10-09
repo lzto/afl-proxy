4a212940d8aa81e34953594be994e5be3a8a4f9d
Tong Zhang <ztong0001@gmail.com>
2021-10-09 16:01:56 -0700

    update script

--- s2e-tools/monitor-s2e-progress.sh
+++ s2e-tools/monitor-s2e-progress.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-name=$1
+d=$1
+name=$2
+
+pushd $d
 
 if [ "$name" == "" ]; then
   name=`pwd|rev|cut -d'/' -f1 |rev `
 fi
 
 find . -name "dmesg*"  -exec grep -i $name {} \;|cut -d']' -f2-|sort |uniq |grep -v enabling
+
+popd
+
