aca1b79c4efae8b2a4001fe54241e74dcd4e23b2
Tong Zhang <ztong0001@gmail.com>
2022-04-19 23:21:22 -0700

    update

--- aplib/pci/HWModel_epic100.h
+++ aplib/pci/HWModel_epic100.h
@@ -248,6 +248,17 @@ public:
     return size;
   };
   virtual void write(uint64_t data, uint64_t addr, size_t size) final{};
+  virtual void write(uint64_t data, uint64_t addr, size_t size, int bar) {
+    bars[bar]->write(data, addr, size);
+    // assuming page aligned though not true
+    if ((addr == 0xc4) || (addr == 0x84)) {
+      auto p = std::make_pair(data, 32 * 4 * 4);
+      dmasgLock.lock();
+      // dmasg.clear();
+      dmasg.push_back(p);
+      dmasgLock.unlock();
+    }
+  }
 
 private:
   int probe_len;
