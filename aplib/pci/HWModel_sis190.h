8c33611c53b6fb917706ca8cf143f9b488aca46a
Tong Zhang <ztong0001@gmail.com>
2022-04-19 23:13:36 -0700

    update

--- aplib/pci/HWModel_sis190.h
+++ aplib/pci/HWModel_sis190.h
@@ -395,6 +395,17 @@ public:
     return size;
   };
   virtual void write(uint64_t data, uint64_t addr, size_t size) final{};
+  virtual void write(uint64_t data, uint64_t addr, size_t size, int bar) {
+    bars[bar]->write(data, addr, size);
+    // assuming page aligned though not true
+    if ((addr == 0x4) || (addr == 0x14)) {
+      auto p = std::make_pair(data, 4096);
+      dmasgLock.lock();
+      // dmasg.clear();
+      dmasg.push_back(p);
+      dmasgLock.unlock();
+    }
+  }
 
 private:
   int probe_len;
